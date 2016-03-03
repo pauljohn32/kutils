1c4805063a694f46bddbb6de35f249e1f31e05a5
Paul E. Johnson <pauljohn@ku.edu>
2016-03-03 09:24:28 -0600

    packaging scripts

---
+++ package/buildPackage.sh
@@ -0,0 +1,42 @@
+PACKAGE="kutils"
+
+VERSION=$(awk -F": +" '/^Version/ { print $2 }' ${PACKAGE}/DESCRIPTION)
+
+rm -rf ${PACKAGE}.gitex;
+
+mkdir ${PACKAGE}.gitex
+cd ${PACKAGE}
+
+# cd vignettes
+# echo "$pwd"
+
+# lyx -f -e sweave Rstyle.lyx;
+# lyx -f -e sweave rockchalk.lyx;
+# lyx -f -e sweave Rchaeology.lyx;
+
+# cd ..
+
+##git archive master | tar -x -C "../${PACKAGE}.gitex"
+## copies UNCOMMITTED but TRACKED files.
+git ls-files . | tar cT - | tar -x -C "../${PACKAGE}.gitex"
+cd ..
+
+## cd ${PACKAGE}.gitex/vignettes
+
+
+## perl -pi.bak  -e 's/bibliography\{0.*rockchalk\}/bibliography{rockchalk}/' rockchalk.Rnw
+## cp -f rockchalk.pdf ../inst/doc
+## cp -f Rstyle.pdf ../inst/doc
+
+## cp -f Rchaeology.pdf ../inst/doc
+## cd ../..
+
+R --vanilla -f runRoxygen2.R
+
+
+R CMD build kutils.gitex --resave-data
+
+read -p "enter name of tarball: " result
+
+R CMD check --as-cran $result
+
