18151489ac39197c2c3b1ed1936078e6ae465fed
Paul E. Johnson <pauljohn@ku.edu>
2016-07-12 18:14:23 -0500

    updatePackages 100x faster now
    Use of lapply was making this horribly awful slow.
    Now vectorized, is super fast.
    
    prevent effort to install if no new packages needed.
    
    insert documentation

--- package/kutils/R/updatePkgDepends.R
+++ package/kutils/R/updatePkgDepends.R
@@ -1,37 +1,73 @@
-##' Install and update packages and their dependencies
+##' Update packages, spot new dependencies, and install them
 ##'
-##' Checks installed packages for package dependencies, updates
+##' Addresses the problem that R's update.packages does not
+##' trigger the installation of packages that are added as new
+##' requirements in existing packages. It checks for existence
+##' of updates, ascertains whether those packages impose new
+##' requirements, and installs any new requirements. Checks
+##' installed packages for package dependencies, updates
 ##' packages, and installs/updates packages dependencies
+##'
+##' This function is valuable in system maintenance because sometimes
+##' existing packages adopt new requirements and the update.packages
+##' function does not notice.  Another possible case would be that a user
+##' accidentally deletes some packages without realizing other packages
+##' depend on them.
+##'
+##' If this is run as the root/administrator privileged, then base R
+##' packages may be updated, but if user is not root/administrator, there
+##' will be a warning that packages were not updates because permissions
+##' were lacking. This warning does not interfere with rest of purpose
+##' of this function, since the new dependencies can be installed in a
+##' place where the user has privileges, either by specifying libnew
+##' as a full directory name or by setting it to NULL, which puts new
+##' packages in $R_LIBS_USER
 ##' @param ask If TRUE, asks user to select packages to update
 ##' @param checkBuilt If TRUE, packages built under earlier versions
 ##'     of R are to be considered 'old'
-##' @return A vector of packages being inserted to complete update
-##' @author Kenna Whitley <kennamarie@@ku.edu>
+##' @param dependencies A vector specifying which type of dependencies
+##'     need to be taken into account. We default to c("Depends",
+##'     "Imports", "LinkingTo").
+##' @param libnew The R library folder into which the new packages
+##'     must be installed.  Defaults to "/usr/share/R/library", which
+##'     is where EL7 likes those things.  To install packages in
+##'     personal user directory, put libnew = NULL.
+##' @param repos A vector of repositories on which to search for
+##'     packages. Same definition as in R's install.packages or
+##'     install.packages.
+##' @param ... additional arguments passed to update.packages and
+##'     install.packages
+##' @return A vector of new packages being installed
+##' @author Paul Johnson <pauljohn@@ku.edu>
 ##' @importFrom tools package_dependencies
 ##' @export
 ##' @importFrom utils available.packages install.packages
-##'           installed.packages packageDescription update.packages
+##'     installed.packages packageDescription update.packages
 ##' @examples
-##' options(repos = c("http://rweb.crmda.ku.edu/kran", "http://rweb.crmda.ku.edu/cran"))
+##' ## options(repos = c("http://rweb.crmda.ku.edu/kran", "http://rweb.crmda.ku.edu/cran"))
 ##' ## not run
-##' ##updatePackages(ask = FALSE)
-##'
-updatePackages <- function(ask = FALSE, checkBuilt = TRUE){
-    pkgdeps <- function(pkg, which = c("Depends", "Imports", "LinkingTo")){
-        deps <- package_dependencies(pkg, db = available.packages(), which = c("Depends", "Imports", "LinkingTo"), recursive = TRUE)
-        grab <- sapply(deps[[1]], packageDescription, fields = "Priority")
-        deps <- c(deps[[1]][is.na(grab)])
-        deps
-    }
+##' ## updatePackages(ask = FALSE)
+##' ## New packages will go to user directory
+##' ## updatePackages(ask = FALSE, libnew = NULL)
+##' ## otherwise must have write privileges in libnew.
+updatePackages <- function(ask = FALSE, checkBuilt = TRUE,
+                           dependencies = c("Depends", "Imports", "LinkingTo"),
+                           libnew = "/usr/share/R/library/",
+                           repos = c("http://rweb.crmda.ku.edu/cran", "http://rweb.crmda.ku.edu/kran"),
+                           ...)
+{
+    ## See if package asks for dependencies not currently installed
     installedPackages <- rownames(installed.packages())
-    targets <- lapply(installedPackages, pkgdeps)
-    targets <- unique(targets)
-    targets <- unlist(targets[lapply(targets,length)>0])
+    update.packages(ask = FALSE, checkBuilt = TRUE, ...)
+    avail <- available.packages(repos = repos)
+    alldeps <- unique(unlist(tools::package_dependencies(installedPackages, which = dependencies,
+                                                         db = avail,
+                                                         recursive = TRUE)))
     
-    alreadyHave <- targets %in% installedPackages
-    lapply(targets[!alreadyHave], install.packages)
-    print(targets[!alreadyHave])
-    
-    update.packages(ask = FALSE, checkBuilt = TRUE)
+    targets <- alldeps[!alldeps %in% installedPackages]
+    if (length(targets) > 0) {
+        install.packages(targets, lib = libnew, repos = repos, dependencies = dependencies, ...)
+        print(paste("These new packages were needed", targets))
+    }
     targets
 }
