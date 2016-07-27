d78239cb7be23240b7cb9e9551c6f9fc1261bec2
Paul E. Johnson <pauljohn@ku.edu>
2016-07-27 11:26:32 -0500

    updatePackages
    * updatePkgDepends.R: Accelerate by vectorization, insert
    BIOC as repo, clean up docs

--- package/kutils/R/updatePkgDepends.R
+++ package/kutils/R/updatePkgDepends.R
@@ -1,13 +1,14 @@
 ##' Update packages, spot new dependencies, and install them
 ##'
-##' Addresses the problem that R's update.packages does not
+##' Addresses the problem that updates for previously installed R
+##' packages may insert new dependencies. R's update.packages does not
 ##' trigger the installation of packages that are added as new
-##' requirements in existing packages. It checks for existence
-##' of updates, ascertains whether those packages impose new
-##' requirements, and installs any new requirements. Checks
-##' installed packages for package dependencies, updates
-##' packages, and installs/updates packages dependencies
+##' requirements in existing packages.
 ##'
+##' This function checks for existence of updates, ascertains whether
+##' those packages impose new requirements, and installs any new
+##' requirements. Then it conducts the update.
+##' 
 ##' This function is valuable in system maintenance because sometimes
 ##' existing packages adopt new requirements and the update.packages
 ##' function does not notice.  Another possible case would be that a user
@@ -15,12 +16,17 @@
 ##' depend on them.
 ##'
 ##' If this is run as the root/administrator privileged, then base R
-##' packages may be updated, but if user is not root/administrator, there
-##' will be a warning that packages were not updates because permissions
-##' were lacking. This warning does not interfere with rest of purpose
-##' of this function, since the new dependencies can be installed in a
-##' place where the user has privileges, either by specifying libnew
-##' as a full directory name or by setting it to NULL, which puts new
+##' packages may be updated, but if user is not root/administrator,
+##' there will be a warning that packages were not updated because
+##' permissions were lacking. For example
+##'
+##' "Warning: package 'boot' in library '/usr/lib/R/library' will not
+##' be updated.
+##' 
+##' This warning does not interfere with rest of purpose of this
+##' function, since the new dependencies can be installed in a place
+##' where the user has privileges, either by specifying libnew as a
+##' full directory name or by setting it to NULL, which puts new
 ##' packages in $R_LIBS_USER
 ##' @param ask If TRUE, asks user to select packages to update
 ##' @param checkBuilt If TRUE, packages built under earlier versions
@@ -46,19 +52,21 @@
 ##' @examples
 ##' ## options(repos = c("http://rweb.crmda.ku.edu/kran", "http://rweb.crmda.ku.edu/cran"))
 ##' ## not run
-##' ## updatePackages(ask = FALSE)
+##' ## updatePackages()
 ##' ## New packages will go to user directory
 ##' ## updatePackages(ask = FALSE, libnew = NULL)
 ##' ## otherwise must have write privileges in libnew.
 updatePackages <- function(ask = FALSE, checkBuilt = TRUE,
                            dependencies = c("Depends", "Imports", "LinkingTo"),
                            libnew = "/usr/share/R/library/",
-                           repos = c("http://rweb.crmda.ku.edu/cran", "http://rweb.crmda.ku.edu/kran"),
+                           repos = c("http://rweb.crmda.ku.edu/cran",
+                                     "http://rweb.crmda.ku.edu/kran",
+                                     "http://www.bioconductor.org/packages/3.3/bioc"),
                            ...)
 {
     ## See if package asks for dependencies not currently installed
     installedPackages <- rownames(installed.packages())
-    update.packages(ask = FALSE, checkBuilt = TRUE, ...)
+    update.packages(ask = FALSE, checkBuilt = TRUE, repos = repos, ...)
     avail <- available.packages(repos = repos)
     alldeps <- unique(unlist(tools::package_dependencies(installedPackages, which = dependencies,
                                                          db = avail,
@@ -69,5 +77,10 @@ updatePackages <- function(ask = FALSE, checkBuilt = TRUE,
         install.packages(targets, lib = libnew, repos = repos, dependencies = dependencies, ...)
         print(paste("These new packages were needed", targets))
     }
-    targets
+    if (length(targets) == 0){
+        print("No new packages were installed to fulfill dependencies")
+        return(NULL)
+    } else {
+        targets
+    }
 }
