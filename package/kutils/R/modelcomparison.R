8bc59aa1ae0262d921a2d1e3c914ccacf03d952e
Ben Kite <bakite@ku.edu>
2017-01-04 11:21:45 -0600

    Shortened the example run time for compareCFA

--- package/kutils/R/modelcomparison.R
+++ package/kutils/R/modelcomparison.R
@@ -71,7 +71,7 @@ detectNested <- function(models){
 ##'     added to the last three columns of the comparison table. The
 ##'     default value is TRUE.
 ##' @param file Default is NULL, no file created. If output file is desired,
-##'     provide a character string for the file name. 
+##'     provide a character string for the file name.
 ##' @author Benjamin Arthur Kite
 ##' @export
 ##' @importFrom stats anova update
@@ -84,13 +84,13 @@ detectNested <- function(models){
 ##' f1 ~~ 1*f1"
 ##' genmodel2 <- "f1 =~ .7*v1 + .7*v2 + .7*v3 + .7*v4 + .7*v5 + .2*v6
 ##' f1 ~~ 1*f1"
-##' 
+##'
 ##' dat1 <- simulateData(genmodel, sample.nobs = 300)
 ##' dat2 <- simulateData(genmodel2, sample.nobs = 300)
 ##' dat1$group <- 0
 ##' dat2$group <- 1
 ##' dat <- rbind(dat1, dat2)
-##'  
+##'
 ##' congModel <- "
 ##'               f1 =~ 1*v1 + v2 + v3 + v4 + v5 + v6
 ##'     		  f1 ~~ f1
@@ -106,35 +106,16 @@ detectNested <- function(models){
 ##'     		  f1 ~~ f1
 ##'     		  f1 ~0*1
 ##'     		"
-##' partialweakModel2 <- "
-##'               f1 =~ 1*v1 + c(L2,L2)*v2 + c(L3,L3)*v3 + c(L4,L4)*v4 + v5 + v6
-##'     		  f1 ~~ f1
-##'     		  f1 ~0*1
-##'     		"
-##' partialstrongModel1 <- "
-##'               f1 =~ 1*v1 + c(L2,L2)*v2 + c(L3,L3)*v3 + c(L4,L4)*v4 + c(L5,L5)*v5 + v6
-##'     		  f1 ~~ f1
-##'     		  f1 ~ c(0,NA)*1
-##'     		  v1 ~ c(I1,I1)*1
-##'     		  v2 ~ c(I2,I2)*1
-##'     		  v3 ~ c(I3,I3)*1
-##'     		  v4 ~ c(I4,I4)*1
-##'     		  v5 ~ c(I5,I5)*1
-##'     		  v6 ~ c(I6,I6)*1
-##'     		"
 ##' cc1 <- cfa(congModel, data=dat, group="group", meanstructure=TRUE, estimator = "MLR")
 ##' cc2 <- cfa(weakModel, data=dat, group="group", meanstructure=TRUE, estimator = "MLR")
 ##' cc21 <- cfa(partialweakModel, data=dat, group="group", meanstructure=TRUE, estimator = "MLR")
-##' cc3 <- cfa(partialstrongModel1, data=dat, group="group", meanstructure=TRUE, estimator = "MLR")
 ##'
-##' models <- list(cc1, cc2, cc21, cc3)
+##' models <- list(cc1, cc2, cc21)
 ##' compareCFA(models, nesting = NULL)
 ##'
-##' models <- list("Configural" = cc1, "Metric" = cc2, "PartialMetric" = cc21, "Scalar" = cc3)
-##' compareCFA(models, nesting = "Configural > Metric + PartialMetric > Scalar")
+##' models <- list("Configural" = cc1, "Metric" = cc2, "PartialMetric" = cc21)
+##' compareCFA(models, nesting = "Configural > Metric + PartialMetric")
 ##'
-##' compareCFA(models, fitmeas = c("chisq", "df", "cfi", "rmsea", "tli"),
-##' nesting = "Configural > Metric + PartialMetric > Scalar")
 ##' \donttest{
 ##' ## Creates output file
 ##' ## compareCFA(models, fitmeas = c("chisq", "df", "cfi", "rmsea", "tli"),
@@ -203,7 +184,7 @@ compareCFA <- function(models,
                 sumtable[i, c("dchi", "ddf", "npval")] <- tmp
                 sumtable[i, "dchi"] <- paste0(sumtable[i, "dchi"], letters[letter])
                 noteinfo[[letter]] <- paste0(letters[letter], " = ", comparison["nested"], " .vs ", comparison["parent"])
-                letter <- letter + 1      
+                letter <- letter + 1
             }
         }
         output <- list(sumtable, unlist(noteinfo))
