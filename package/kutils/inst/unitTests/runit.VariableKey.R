df451c033648a04cf45a2972770313f33c430cf5
Charlie Redmon <redmon@ku.edu>
2017-08-04 14:52:01 -0500

    began extension of keyApply unit tests to conversion by coercion

--- package/kutils/inst/unitTests/runit.VariableKey.R
+++ package/kutils/inst/unitTests/runit.VariableKey.R
@@ -70,19 +70,31 @@ test.keyApply <- function() {
     ll0 <- ifelse(l == FALSE, TRUE, FALSE)
     checkEquals(ll0, df1[,"varLL"])  #compare code version with VarKey version
 
-    ## logical --> integer
+    ## logical --> integer (1)
+    li0 <- as.integer(l)
+    checkEquals(li0, df1[,"varLI1"])
+
+    ## logical --> integer (2)
     li0 <- ifelse(l == FALSE, 0, 1)
-    checkEquals(li0, df1[,"varLI"])
+    checkEquals(li0, df1[,"varLI2"])
 
-    ## logical --> numeric
+    ## logical --> numeric (1)
+    ln0 <- as.numeric(l)
+    checkEqualsNumeric(ln0, df1[,"varLN1"], tolerance=floatPrecision)
+    
+    ## logical --> numeric (2)
     ln0 <- ifelse(l == FALSE, -0.5, 0.5)
-    checkEqualsNumeric(ln0, df1[,"varLN"], tolerance=floatPrecision)
+    checkEqualsNumeric(ln0, df1[,"varLN2"], tolerance=floatPrecision)
 
-    ## logical --> factor
+    ## logical --> factor (1)
+    lf0 <- factor(l)
+    checkEquals(lf0, df1[,"varLF1"])
+    
+    ## logical --> factor (2)
     lf0 <- factor(ifelse(l == FALSE, "no", "yes"), levels=c("yes","no"))
-    checkEquals(lf0, df1[,"varLF"])
+    checkEquals(lf0, df1[,"varLF2"])
 
-    ## logical --> ordinal (FAIL DUE TO KEYAPPLY HANDLING OF ORDERED CLASS)
+    ## logical --> ordinal
     lo0 <- ordered(ifelse(l == FALSE, "fail", "pass"))
     lo1 <- df1[,"varLO"]
     checkEquals(lo0, lo1)
@@ -94,11 +106,15 @@ test.keyApply <- function() {
     ## TEST CONVERSIONS FROM INTEGER
     i1 <- df0[,"varI1"]
     i2 <- df0[,"varI2"]
+    
+    ## integer --> logical (1) FAIL
+    il0 <- as.logical(ifelse(i1 >= 999, NA, i1))
+    checkEquals(il0, df1[,"varIL1"])
 
-    ## integer --> logical
+    ## integer --> logical (2)
     il0 <- as.logical(ifelse(i2 == 1, TRUE, FALSE))
-    checkEquals(il0, df1[,"varIL"])
-
+    checkEquals(il0, df1[,"varIL2"])
+    
     ## integer --> integer
     ii0 <- ifelse(i1 >= 999, NA, i1)
     checkEquals(ii0, df1[,"varII"])
@@ -111,7 +127,7 @@ test.keyApply <- function() {
     if0 <- factor(ifelse(i2 < 3, "nonHOV", "HOV"), levels=c("nonHOV","HOV"))
     checkEquals(if0, df1[,"varIF"])
 
-    ## integer --> ordinal (FAIL DUE TO KEYAPPLY HANDLING OF ORDERED CLASS)
+    ## integer --> ordinal
     io0 <- ordered(plyr::mapvalues(i2, from=1:5,
                    to=c("unfavorable", "slightlyUnfavorable", "neutral",
                         "slightlyFavorable", "favorable")),
@@ -132,9 +148,13 @@ test.keyApply <- function() {
     nl0 <- ifelse(n2 < 0.5, FALSE, TRUE)
     checkEquals(nl0, df1[,"varNL"])
 
-    ## numeric --> integer (FAIL: RETAINS NUMERIC CLASS)
+    ## numeric --> integer (1)
     ni0 <- as.integer(ifelse(n1 <= -999, NA, n1))
-    checkEquals(ni0, df1[,"varNI"])
+    checkEquals(ni0, df1[,"varNI1"])
+    
+    ## numeric --> integer (2)
+    ni0 <- as.integer(ifelse(n2 < 0.5, 0, 1))
+    checkEquals(ni0, df1[,"varNI2"])
 
     ## numeric --> numeric
     nn0 <- ifelse(n1 <= -999, NA, n1)
@@ -146,16 +166,20 @@ test.keyApply <- function() {
                   to=c(rep("Q1", 3), rep(paste0("Q",2:5), each=2))))
     checkEquals(nf0, df1[,"varNF"])
     
-    ## numeric --> ordinal (FAIL DUE TO KEYAPPLY HANDLING OF ORDERED CLASS)
+    ## numeric --> ordinal
     no1 <- ordered(plyr::mapvalues(as.character(n2),
                    from=as.character(seq(0, 1, 0.1)),
                    to=c(rep("lower",5), rep("middle",4), rep("upper",2))))
     checkEquals(no1, df1[,"varNO"])
-    
-    ## numeric --> character
-    nc1 <- plyr::mapvalues(as.character(n2), from=as.character(seq(0, 1, 0.1)),
+
+    ## numeric --> character (2)
+    nc0 <- as.character(n1)
+    checkEquals(nc0, df1[,"varNC1"])
+
+    ## numeric --> character (2)
+    nc0 <- plyr::mapvalues(as.character(n2), from=as.character(seq(0, 1, 0.1)),
                            to=c("A","B","C","D","E","F","G","H","I","J","K"))
-    checkEquals(nc1, df1[,"varNC"])
+    checkEquals(nc0, df1[,"varNC2"])
 
     ## TEST CONVERSIONS FROM FACTOR
     f1 <- df0[,"varF1"]
@@ -181,7 +205,7 @@ test.keyApply <- function() {
     ff0 <- plyr::mapvalues(f3, from=levels(f3), to=c(rep("P", 4), "F"))
     checkEquals(ff0, df1[,"varFF"])
 
-    ## factor --> ordinal (FAIL DUE TO KEYAPPLY HANDLING OF ORDERED CLASS)
+    ## factor --> ordinal
     ff0 <- ordered(f2, levels=c("lo","med","hi"))
     checkEquals(ff0, df1[,"varFO"])
 
@@ -228,17 +252,20 @@ test.keyApply <- function() {
     ci0 <- ifelse(c1 %in% c("0","0.1","0.2","0.3","0.4"), 0, 1)
     checkEquals(ci0, df1[,"varCI"])
 
-    ## character --> numeric (FAIL: RETAINS CHARACTER CLASS)
-    ## NOTE: value_new must be empty to prevent "logic error" in keyApply
+    ## character --> numeric (1)
     cn0 <- as.numeric(c1)
-    checkEqualsNumeric(cn0, df1[,"varCN"], tolerance=floatPrecision)
+    checkEqualsNumeric(cn0, df1[,"varCN1"], tolerance=floatPrecision)
+    
+    ## character --> numeric (2)
+    cn0 <- as.numeric(c1)
+    checkEqualsNumeric(cn0, df1[,"varCN2"], tolerance=floatPrecision)
 
     ## character --> factor
     cf0 <- factor(plyr::mapvalues(c1, from=as.character(seq(0,1,0.1)),
                   to=c("X","X","X","X","Y","Y","Y","Y","Z","Z","Z")))
     checkEquals(cf0, df1[,"varCF"])
 
-    ## character --> ordinal (FAIL DUE TO KEYAPPLY HANDLING OF ORDERED CLASS)
+    ## character --> ordinal
     co0 <- ordered(plyr::mapvalues(c1, from=as.character(seq(0,1,0.1)),
                    to=c("F","F","F","F","F","F","D","C","B","A","A")),
                    levels=c("F","D","C","B","A"))
