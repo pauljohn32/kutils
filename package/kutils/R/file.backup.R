61b53b62761e16449be9db2e524eecdb4c034fe1
Paul E. Johnson <pauljohn@ku.edu>
2018-09-13 15:29:47 -0500

    file.backup: insert keep.old paramter, default as FALSE now.
    file.backup: check if file exists before trying to back it up
    
    file.backup
    
    Fixed problem with "file not found" on fullpath example,
    insert new verbose argument, check examples.
    
    file.backup: beautify verbose output

--- package/kutils/R/file.backup.R
+++ package/kutils/R/file.backup.R
@@ -9,8 +9,13 @@
 ##' full directory name will be created and returned.
 ##' 
 ##' @param name A character string for the name of the file.
-##' @param fullpath Return the full directory path to the file. Default FALSE
-##' @author Shadi Pirhosseinloo <shadi@@ku.edu>
+##' @param fullpath Return the full directory path to the
+##'     file. Default FALSE, return only the file name.
+##' @param keep.old If FALSE (default), rename the file. Otherwise, keep old copy.
+##' @param verbose If TRUE, list the files in the output directory
+##' @param keepold Default is FALSE. FALSE means rename the old file.
+##'     TRUE: copy to backup name, leave old version.
+##' @author Shadi Pirhosseinloo <shadi@@ku.edu> Paul Johnson <pauljohn@@ku.edu>
 ##' @return The name of the newly created file.
 ##' @importFrom base format
 ##' @importFrom tools file_ext
@@ -25,34 +30,60 @@
 ##' system("touch test.2.txt")
 ##' system("touch test.3.txt")
 ##' system("touch test.4.txt")
+##' system("touch test.5.txt")
+##' ## note: no extension next
+##' system("touch test.6")
 ##' list.files()
 ##' file.backup("test.1.txt")
 ##' file.backup("test.2.txt", fullpath=TRUE)
 ##' list.files()
 ##' setwd(owd)
 ##' file.backup(file.path(tdir, "test.3.txt"))
-##' ## Should be same path because input had a full path
+##' ## Next should be same path because input had a full path
 ##' file.backup(file.path(tdir, "test.4.txt"), fullpath=TRUE)
-file.backup <- function(name, fullpath = FALSE){
-  date_cr <- base::format(base::file.info(name)$mtime, "%Y%m%d-%H%M")
-  ext_name <- tools::file_ext(name)
-  noext_name <- tools::file_path_sans_ext(name)
-  new_name <- paste0(noext_name, "-", date_cr, ".", ext_name)
-
-  ## Abort if new file name already exists
-  if (file.exists(new_name)){
-      MESSG <- paste("file.backup did nothing: backup file already exists")
-      warning(MESSG)
-  }
-  ret <- file.rename(name, new_name)
-  if(!ret) {
-      MESSG <- paste("file.rename(", name, ",", new_name, "failed")
-      stop(MESSG)
-  }
-  if(fullpath){
-      return(normalizePath(new_name))
-  }
-  ## else, give back adjusted with same path information it had
-  new_name
+##' file.backup(file.path(tdir, "test.5.txt"), fullpath = TRUE, verbose = TRUE)
+##' file.backup(file.path(tdir, "test.6"))
+file.backup <- function(name, fullpath = FALSE, keep.old = FALSE, verbose = FALSE){
+    if(!file.exists(name)){
+        MESSG <- paste("file", name, "does not exist. No backup created.")
+        warning(MESSG)
+        return(NULL)
+    }
+    dir.source <- dirname(normalizePath(name))
+    
+    date_cr <- base::format(base::file.info(name)$mtime, "%Y%m%d-%H%M")
+    ext_name <- tools::file_ext(name)
+    noext_name <- tools::file_path_sans_ext(name)
+    new_name <- paste0(noext_name, "-", date_cr,
+                       if(!ext_name == "") {paste0( ".", ext_name)})
+   
+    ## Abort if new file name already exists
+    if (file.exists(new_name)){
+        MESSG <- paste("backup file already exists. No new backup created.")
+        warning(MESSG)
+        return(new_name)
+    }
+    ret <- if(keep.old){
+               file.copy(name, new_name, recursive = TRUE, overwrite = TRUE,
+                         copy.mode = TRUE, copy.date = TRUE)
+           }else{
+               file.rename(name, new_name)
+           }
+    if(!ret) {
+        MESSG <- paste("file.rename(", name, ",", new_name, "failed")
+        stop(MESSG)
+    }
+    if(verbose){
+        cat("Full file list of directory:", dir.source, "\n")
+        print(list.files(dir.source))
+        cat("End of file list\n")
+    }
+    if(fullpath){
+        new_name_fullpath <- normalizePath(new_name)
+        return(new_name_fullpath)
+    }
+    
+    ## else, give back adjusted with same path information it had
+    new_name
 }
 
