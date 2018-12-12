ca909f3ae9bb5860dbbbb12c98026d7da21a1f6e
Paul E. Johnson <pauljohn@ku.edu>
2018-12-12 10:47:09 -0600

    importQualtrics: remove reliance on data.table
    importQualtrics: the read.csv alternative corrected

--- package/kutils/R/importQualtrics.R
+++ package/kutils/R/importQualtrics.R
@@ -23,7 +23,7 @@
 ##' @param stringsAsFactors Default FALSE, same meaning as R's
 ##'     read.csv. Does not affect importation of Excel files.
 ##' @importFrom openxlsx read.xlsx
-##' @importFrom data.table fread
+##' @importFrom utils type.convert
 ##' @export
 ##' @return Data frame that has attribute "questions" if questrow is
 ##'     specified.
@@ -31,42 +31,27 @@
 importQualtrics <- function (file, namerow = 1, skip = 3, questrow = NULL,
                              dropTEXT = TRUE, stringsAsFactors = FALSE){
     if (length(grep("csv$", tolower(file))) > 0) {
-        ## dat1 <- read.csv(file,
-        ##                  stringsAsFactors = stringsAsFactors,
-        ##                  header = FALSE)
-        ## dat2 <- read.csv(file,
-        ##                  stringsAsFactors = stringsAsFactors,
-        ##                  skip = skip, header = FALSE)
-
-        dat1 <- data.table::fread(file, header = FALSE)
-        dat1.colnames <- colnames(dat1[1, ])
-        dat1.4.1 <- as.character(dat1[4, 1])
-
-        dat2 <- data.table::fread(file, skip = 3, header = FALSE)
-        skip.new <- skip
-        while(skip.new < NROW(dat1) && !isTRUE(all.equal(dat1.4.1, as.character(dat2[1,1])))){
-            skip.new <- skip.new + 1
-            dat2 <- fread(file, skip = skip.new, header = FALSE,
-                        strip.white = TRUE, blank.lines.skip = TRUE)
-        }
-        MESSG1 <- paste("fread required skip = ", skip, "to manage importation")
-        if(skip > 3) warning(MESSG1)
-
+        dat1 <- read.csv(file, allowEscapes=TRUE, fill=TRUE,
+                         header = FALSE, stringsAsFactors = stringsAsFactors)
+        lastbadrow <- grep("\\{\"ImportId", dat1[ , 1])
+        if(lastbadrow != skip) stop(paste("check data: is last non-data row", skip, "?"))
+        d2.1 <- dat1[ -(1:lastbadrow), ]
+        dat2 <- as.data.frame(lapply(d2.1, type.convert, as.is = TRUE))
+                
         if(NROW(dat2) != NROW(dat1) - skip){
             MESSG2 <- paste("Check number of rows in imported data")
             warning(MESSG2)
         } else {
-            MESSG3 <- paste("\n NOTE: importQualtrics: The warnings from fread are",
-                            "probably harmless.\n", "Those warnings",
-                            "result when a cell has an embedded carriage return.\n",
-                            "Inspect output carefully.")
+            MESSG3 <- paste("\n NOTE:",
+                            "Compare rows between input and output files.\n")
             warning(MESSG3)
         }
-        dat2 <- as.data.frame(dat2)
     } else {
         startRow <- skip + 1
         dat1 <- read.xlsx(file, skipEmptyCols = FALSE,
                           colNames = FALSE)
+        lastbadrow <- grep("\\{\"ImportId", dat1[ , 1])
+        if(lastbadrow != skip) stop(paste("check data: is last non-data row", skip, "?\n"))
         dat2 <- read.xlsx(file, startRow = startRow,
                           skipEmptyCols = FALSE, colNames = FALSE)
     }
